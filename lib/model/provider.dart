cfd830d9356b3945c4b4ef92fc6bd5ed991baff1
anuj-1637 <anuj94460@gmail.com>
2025-08-15 22:34:11 +0530

    food app

--- lib/model/provider.dart
+++ lib/model/provider.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
 
-class databaseProvider extends ChangeNotifier {}
+class databaseProvider extends ChangeNotifier {
+  late List _data;
+  List get data => _data;
+  var box = Hive.box('foodDataBox');
+
+  addData() {
+    _data = box.values.toList();
+    notifyListeners();
+  }
+}
